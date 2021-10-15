fa2aabd199025a1de1dee390c75b4830df5a2585
Salih Güngörmez <salihgungormez@outlook.com>
2021-10-15 12:03:57 +0300

    Initial commit

---
+++ alan.js
@@ -0,0 +1,9 @@
+var yaricap;
+function hesapla(alan, cevre) {
+    alan = (Math.PI * (yaricap * yaricap))
+    cevre = (Math.PI * 2 * yaricap);
+    console.log(`yarıçapı ${yaricap} olan dairenin alanı : ${alan} cevresi: ${cevre}`);
+}
+
+hesapla(yaricap = process.argv.slice(2))
+
